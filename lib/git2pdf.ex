e3ec501e9ba0eb3469aa89537551b479ae667de5
Hector Kemp <hectorakemp@gmail.com>
2022-01-26 22:16:58 +1100

    Add expanded ls

--- lib/git2pdf.ex
+++ lib/git2pdf.ex
@@ -5,26 +5,45 @@ defmodule Git2pdf do
   Read a git repo
   Try printing out files in their entirety??
   use gitignore
+  for a folder, print all
+  use wkhtmltopdf
+  table of contents?
+  sorted directory, then files (alphabetically)
+  xslt
+  only use <tbody print style from github>
+  use https://github.com/gutschilla/elixir-pdf-generator
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Git2pdf.hello()
-      :world
-
+  Converts a .gitignore file into a list of relative string filenames.
   """
-  def hello do
-    :world
-  end
-
   def gitignore_file_list(path) do
     File.stream!(path) |>
       Stream.map(&String.trim(&1)) |>
       Stream.filter(&String.length(&1) > 0) |>
-      Stream.filter(fn s -> !String.starts_with?(s, "#") end) |>
+      Stream.filter(&!String.starts_with?(&1, "#")) |>
       Enum.to_list
+  end
+
+  @doc """
+    Recursively walks a directory structure sorted by and builds a tree
+    structure, where folders are branches, and files are leaves.
+    If
+  """
+  # def directory_files_tree() do
+  #   if
+  #   File.ls |>
+  #     Kernel.then(fn {:ok, list} -> list end) |>
+  # end
+  #
+  def expanded_ls(path) do
+    File.ls(path)
+    |> then(fn {:ok, res} -> res end)
+    |> Enum.map(&Path.expand(&1))
+  end
+
+  def sort_folders_first_then_files(ls_result) do
+    %{true: folders, false: files} = Enum.group_by(ls_result, &File.dir?(&1))
+    Enum.concat(Enum.sort(folders), Enum.sort(files))
   end
 end
