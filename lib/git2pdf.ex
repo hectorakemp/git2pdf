9f99c71a8a9001c83cb72fd00f184974ba161837
Hector Kemp <hectorakemp@gmail.com>
2022-01-30 17:06:19 +1100

    First onyx commit

--- lib/git2pdf.ex
+++ lib/git2pdf.ex
@@ -13,28 +13,40 @@ defmodule Git2pdf do
   only use <tbody print style from github>
   use https://github.com/gutschilla/elixir-pdf-generator
   """
+  
 
   @doc """
   Converts a .gitignore file into a list of relative string filenames.
   """
-  def gitignore_file_list(gitignore_path) do
-    File.stream!(gitignore_path) |>
+  def gitignore_file_list do
+    File.stream!(".gitignore") |>
       Stream.map(&String.trim(&1)) |>
       Stream.filter(&String.length(&1) > 0) |>
       Stream.filter(&!String.starts_with?(&1, "#")) |>
       Enum.to_list
   end
 
-  def filter_gitignore(file_list, gitignore_list) do
+  def filter_gitignore(file_list) do
     file_list |>
       Enum.filter(
         fn file ->
           not Enum.any?(
-            gitignore_list,
+            gitignore_file_list(),
             fn g -> String.contains?(file, g) end
           )
         end
       )
+  end
+
+  def build_directory_tree(node, path) do
+    if File.dir? path do
+      node_val = expanded_ls(path) |>
+		 filter_gitignore |> 
+		 Enum.reduce(%{}, fn p, node -> build_directory_tree(node, p) end)
+      Map.put(node, path, node_val)
+    else
+      Map.put(node, path, nil)
+    end
   end
 
   @doc """
@@ -42,20 +54,16 @@ defmodule Git2pdf do
     structure, where folders are branches, and files are leaves.
     If
   """
-  # def directory_files_tree() do
-  #   if
-  #   File.ls |>
-  #     Kernel.then(fn {:ok, list} -> list end) |>
-  # end
-  #
   def expanded_ls(path) do
     File.ls(path)
     |> then(fn {:ok, res} -> res end)
     |> Enum.map(&Path.expand(&1))
   end
-
   def sort_folders_first_then_files(ls_result) do
-    %{true: folders, false: files} = Enum.group_by(ls_result, &File.dir?(&1))
+    %{true: folders, false: files} = Enum.group_by(
+      ls_result, 
+      &File.dir?(&1)
+    )
     Enum.concat(Enum.sort(folders), Enum.sort(files))
   end
 end
